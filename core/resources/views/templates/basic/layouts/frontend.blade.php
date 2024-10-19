05b718a5ab3467e6fe0cdba296052433b12cc86b
Kyekyeku-tech <safooppong121@gmail.com>
2024-10-19 14:41:47 +0000

    update

---
+++ core/resources/views/templates/basic/layouts/frontend.blade.php
@@ -0,0 +1,5 @@
+@extends($activeTemplate.'layouts.app')
+@section('panel')
+
+
+@endsection
---
+++ core/storage/framework/views/e4da9c8f90a6154f5dc4bbf4202f4cca92edc608.php
@@ -0,0 +1,3 @@
+<?php echo  loadExtension('tawk-chat') ?>
+<?php echo  loadExtension('google-analytics') ?>
+<?php /**PATH /home/foryouxy/h5.foryou4.xyz/core/resources/views/partials/plugins.blade.php ENDPATH**/ ?>
